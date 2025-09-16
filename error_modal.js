4b2119356497ad70979656bcf019af037ababa42
Yulia Antiuk <uliaantiuk@gmail.com>
2025-09-16 13:48:02 +0300

    Added modals

---
+++ error_modal.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const nameModal = document.getElementById("nameModal");
+const passwordModal = document.getElementById("passwordModal");
+
+const closes = document.querySelectorAll(".close");
+
+document.getElementById("submit-btn").addEventListener("click", function(e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (!username) {
+        nameModal.style.display = "block";
+      } else if (username && !password) {
+        passwordModal.style.display = "block";
+      } else {
+        alert("Form submitted successfully!");
+      }
+
+});
+
+    closes.forEach(btn => {
+      btn.onclick = function() {
+        btn.closest(".modal").style.display = "none";
+      }
+    });
+
+    window.onclick = function(event) {
+      if (event.target.classList.contains(".modal")) {
+        event.target.style.display = "none";
+      }
+    }});
